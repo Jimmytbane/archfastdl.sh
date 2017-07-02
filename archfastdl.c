6894c85b6a280f07e2c2211ee595fd210a5fad5b
JimmyBot <jimmybot@teknik.io>
2017-07-02 06:09:59 -0400

    verbosity not needed

--- archfastdl.c
+++ archfastdl.c
@@ -2,6 +2,6 @@
 #include<stdlib.h>
 
 int main() {
-  system("sudo reflector --age 1 --protocol https --verbose --latest 2 --sort rate --save /etc/pacman.d/mirrorlist");
+  system("sudo reflector --age 1 --protocol https --latest 2 --sort rate --save /etc/pacman.d/mirrorlist");
   return 0;
 }
