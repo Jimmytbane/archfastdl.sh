a21327f2c797fbd97befb2c609d60799be9a0578
JimmyBot <jimmybot@teknik.io>
2017-07-06 00:22:44 -0400

    forgot fflushes, hopefully this works

--- install.c
+++ install.c
@@ -3,7 +3,10 @@
 
 int main(void) {
   printf("Installing archfastdl, requires sudo to copy to /usr/local/bin");
+  fflush(stdout);
   system("make archfastdl");
+  fflush(stdout);
   system("sudo cp archfastdl /usr/local/bin/archfastdl");
+  fflush(stdout);
   printf("archfastdl installed.");
 }
